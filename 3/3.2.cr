86f4b497a38d6238bac9b756d1e4518f4eb05c02
Ary Borenszweig <asterite@gmail.com>
2021-12-03 16:44:01 -0300

    Rename

--- 3/3.2.cr
+++ 3/3.2.cr
@@ -12,16 +12,16 @@ p! ogr_rating, co2_rating
 puts ogr_rating * co2_rating
 
 def compute_rating(bit_sets)
-  ogr_bit_sets = bit_sets.dup
+  bit_sets = bit_sets.dup
 
   index = 0
 
-  until ogr_bit_sets.size == 1
-    ones, zeros = ogr_bit_sets.partition { |bits| bits[index] == '1' }
-    ogr_bit_sets = yield ones, zeros
+  until bit_sets.size == 1
+    ones, zeros = bit_sets.partition { |bits| bits[index] == '1' }
+    bit_sets = yield ones, zeros
 
     index = (index + 1) % bit_sets.first.size
   end
 
-  ogr_bit_sets.first.to_i(2)
+  bit_sets.first.to_i(2)
 end
